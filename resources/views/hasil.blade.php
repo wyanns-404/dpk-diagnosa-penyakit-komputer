fb5e63c2e5f35e4e0a95f6abe83b4b56e291fa11
Wayan Santie Arif <wayansantiearif@gmail.com>
2024-12-22 20:23:47 +0700

    Update UI

--- resources/views/hasil.blade.php
+++ resources/views/hasil.blade.php
@@ -5,37 +5,39 @@
 @endsection
 
 @section('content')
-<div class="row justify-content-center w-100 flex-wrap gap-3">
-    @foreach ($hasil as $h)
-        @if ($h['probabilitas'] > 0)
-        <div class="box">
-            <div class="content">
-                <div class="percent" data-text="{{ round($h['probabilitas'] * 100, 2) }}%" style="--num: {{ round($h['probabilitas'] * 100) }}">
-                    <div class="dot"></div>
-                    <svg>
-                        <circle cx="70" cy="70" r="70"></circle>
-                        <circle cx="70" cy="70" r="70"></circle>
-                    </svg>
-                </div>
-                <div class="number">
-                    <h2>{{ $h['kerusakan'] }}</h2>
+<div class="container px-4 px-lg-5 d-flex flex-column h-100 align-items-center justify-content-center">
+    <div class="row justify-content-center w-100 flex-wrap gap-3">
+        @foreach ($hasil as $h)
+            @if ($h['probabilitas'] > 0)
+            <div class="box">
+                <div class="content">
+                    <div class="percent" data-text="{{ round($h['probabilitas'] * 100, 2) }}%" style="--num: {{ round($h['probabilitas'] * 100) }}">
+                        <div class="dot"></div>
+                        <svg>
+                            <circle cx="70" cy="70" r="70"></circle>
+                            <circle cx="70" cy="70" r="70"></circle>
+                        </svg>
+                    </div>
+                    <div class="number">
+                        <h2>{{ $h['kerusakan'] }}</h2>
+                    </div>
                 </div>
             </div>
-        </div>
-        @endif
-    @endforeach
-</div>
-
-@if (!empty($hasil) && $hasil[0]['probabilitas'] > 0)
-<div class="row justify-content-center w-100">
-    <div class="hasil p-3">
-        Kerusakan Pada <b>{{ $hasil[0]['kerusakan'] }}</b>
+            @endif
+        @endforeach
     </div>
-</div>
 
-<div class="row justify-content-center w-100">
-    <div class="hasil mt-3 p-3">
-        <b>Solusi : </b> {{ $hasil[0]['solusi'] }}
+    @if (!empty($hasil) && $hasil[0]['probabilitas'] > 0)
+    <div class="row justify-content-center w-100">
+        <div class="hasil p-3">
+            <b>{{ $hasil[0]['kerusakan'] }}</b>
+        </div>
+    </div>
+
+    <div class="row justify-content-center w-100">
+        <div class="hasil mt-3 p-3">
+            <b>Solusi : </b> {{ $hasil[0]['solusi'] }}
+        </div>
     </div>
 </div>
 @endif
